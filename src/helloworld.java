f7808f4b82b515932fb6dc6336e07a5a340dabd9
Tran Van Do <dotv182@gmail.com>
2022-07-04 22:33:43 +0700

    update

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class helloworld {
-    public static void main(String[] args) {
+    public static void helloWorld(){
         System.out.println("hello world");
+    }
+    public static void sumDaySo(int first,int last){
+        int sum = 0;
+        System.out.println("nhap vao so dau tien trong day: ");
+        first = new Scanner(System.in).nextInt();
+        System.out.println("nhap vao so cuoi cung trong day: ");
+        last = new Scanner(System.in).nextInt();
+        while(first > last){
+            System.out.println("vui long nhap lai so dau va so cuoi");
+            System.out.println("nhap vao so dau tien trong day: ");
+            first = new Scanner(System.in).nextInt();
+            System.out.println("nhap vao so cuoi cung trong day: ");
+            last = new Scanner(System.in).nextInt();
+        }
+        for(int i = first ; i<=last;i++){
+            sum += i;
+        }
+        System.out.println("tong cua day tu "+first+ " den " + last +" la: " + sum);
+    }
+
+    public static void main(String[] args) {
+        helloWorld();
+        sumDaySo(1,50);
     }
 }
--- src/sum1to50.java
+++
@@ -1,9 +0,0 @@
-public class sum1to50 {
-    public static void main(String[] args) {
-        int sum = 0;
-        for(int i = 1;i<=50;i++){
-            sum += i;
-        }
-        System.out.println("tong tu 1 den 50 la : " + sum);
-    }
-}
